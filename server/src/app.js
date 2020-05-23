6c853f4fe314d3371a2888d049bfa91d25991264
jeanlee <hyunjinlee@agreable.co>
2020-05-23 23:00:30 +0900

    feat: add cors on backend code

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import passport from "passport";
 import session from "express-session";
+import cors from "cors";
 import models from "./models";
 import route from "./routes/index";
 import config from "./lib/variables/config";
@@ -11,7 +12,9 @@ import logger from "./lib/utils/logger";
 const app = express();
 const port = config.PORT || 4000;
 
-app.use(session({ secret: "SECRET_CODE", resave: true, saveUninitialized: false })); // 세션 연결
+app.use(cors());
+
+app.use(session({ secret: "SECRET_CODE", resave: true, saveUninitialized: false }));
 app.use(passport.initialize()); // passport 구동
 app.use(passport.session()); // 세션 연결
 setupPassport(passport);
